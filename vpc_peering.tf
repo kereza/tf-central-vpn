4d27184843c2d9a0a0ce68e6b2aa114b5baf3835
Ivan Kerezov <ivan.kerezov@tis.biz>
2021-08-20 12:21:42 +0200

    peering route

--- vpc_peering.tf
+++ vpc_peering.tf
@@ -18,3 +18,30 @@ resource "aws_vpc_peering_connection" "keri_org" {
     allow_remote_vpc_dns_resolution = true
   }
 }
+
+resource "aws_route" "private_table_1" {
+  for_each                  = var.remote_vpcs
+  route_table_id            = module.vpc.private_route_table_ids[0]
+  destination_cidr_block    = each.value.cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.keri_org[each.key].id
+  depends_on                = [module.vpc.private_route_table_ids]
+
+  timeouts {
+    create = "5m"
+    delete = "5m"
+  }
+}
+
+resource "aws_route" "private_table_2" {
+  for_each                  = var.remote_vpcs
+  route_table_id            = module.vpc.private_route_table_ids[1]
+  destination_cidr_block    = each.value.cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.keri_org[each.key].id
+  depends_on                = [module.vpc.private_route_table_ids]
+
+  timeouts {
+    create = "5m"
+    delete = "5m"
+  }
+}
+
