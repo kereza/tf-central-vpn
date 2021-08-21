6becd6b964608d362759c8b17e42dbc7b04815dc
Ivan Kerezov <ivan.kerezov@tis.biz>
2021-08-21 15:34:35 +0200

    add README

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # tf-central-vpn
+
+This repo is part of series of personal repos where I do tests on the following topics:
+
+1. Terraform Cloud CI/CD pipeline based on GitHub Actions for central infrastructure and release management and team collaboration
+2. Managing and Deploying EKS cluster (dev and prod stages)
+3. Deploying test apps on the EKS cluster (dev and prod stages)
+4. Central AWS VPC (peered with PROD and DEV VPCs), used for Client VPN connection with Certificase based authentication
+5. GitHub Actions running in every repo with special rules to check the Terrafrom code before merging branches (more check can be added)
+
+This particular repo is for setting up the AWS Central Client VPN based on Certificase for authentication. If we need private connection to the VPC's in the organizations - we can use it (Of course in the real world you will probably have site-to-site VPN or even AWS Direct Connect). It setup a so called "shared VPC" which is peered with all the other VPCs in the Organization and creates the necessary routing tables and Security Groups.
--- vpn.tf
+++ vpn.tf
@@ -17,30 +17,30 @@ resource "aws_ec2_client_vpn_endpoint" "client_vpn" {
   tags = { Name = "kerezov-clientvpn-${var.env}" }
 }
 
-resource "aws_ec2_client_vpn_network_association" "client_vpn" {
-  count                  = length(module.vpc.private_subnets)
-  client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.client_vpn.id
-  subnet_id              = module.vpc.private_subnets[count.index]
-  security_groups        = [module.sg_vpn.this_security_group_id]
-}
+# resource "aws_ec2_client_vpn_network_association" "client_vpn" {
+#   count                  = length(module.vpc.private_subnets)
+#   client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.client_vpn.id
+#   subnet_id              = module.vpc.private_subnets[count.index]
+#   security_groups        = [module.sg_vpn.this_security_group_id]
+# }
 
-resource "aws_ec2_client_vpn_authorization_rule" "client_vpn" {
-  for_each               = toset(concat([var.cidr], values(var.remote_vpcs)[*]["cidr"]))
-  client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.client_vpn.id
-  target_network_cidr    = each.key
-  authorize_all_groups   = true
-}
+# resource "aws_ec2_client_vpn_authorization_rule" "client_vpn" {
+#   for_each               = toset(concat([var.cidr], values(var.remote_vpcs)[*]["cidr"]))
+#   client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.client_vpn.id
+#   target_network_cidr    = each.key
+#   authorize_all_groups   = true
+# }
 
-resource "aws_ec2_client_vpn_route" "routes_subnet_1" {
-  for_each               = toset(values(var.remote_vpcs)[*]["cidr"])
-  client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.client_vpn.id
-  destination_cidr_block = each.key
-  target_vpc_subnet_id   = aws_ec2_client_vpn_network_association.client_vpn[0].subnet_id
-}
+# resource "aws_ec2_client_vpn_route" "routes_subnet_1" {
+#   for_each               = toset(values(var.remote_vpcs)[*]["cidr"])
+#   client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.client_vpn.id
+#   destination_cidr_block = each.key
+#   target_vpc_subnet_id   = aws_ec2_client_vpn_network_association.client_vpn[0].subnet_id
+# }
 
-resource "aws_ec2_client_vpn_route" "routes_subnet_2" {
-  for_each               = toset(values(var.remote_vpcs)[*]["cidr"])
-  client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.client_vpn.id
-  destination_cidr_block = each.key
-  target_vpc_subnet_id   = aws_ec2_client_vpn_network_association.client_vpn[1].subnet_id
-}
+# resource "aws_ec2_client_vpn_route" "routes_subnet_2" {
+#   for_each               = toset(values(var.remote_vpcs)[*]["cidr"])
+#   client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.client_vpn.id
+#   destination_cidr_block = each.key
+#   target_vpc_subnet_id   = aws_ec2_client_vpn_network_association.client_vpn[1].subnet_id
+# }
